9540fa470e35c1c6f13e701e9d7b9e0484726c7b
Louis BRAZIER <brazier.louis@gmail.com>
2017-11-21 01:46:59 -0500

    Updated Angular and Node server to setup the proxy and establish connection with remote API.

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ var express = require('express'); // call express
 var app = express(); // define the app
 var bodyParser = require('body-parser');
 var path = require("path"); // Provide utilities for working with file and directory paths
+var http = require("https");
+var querystring = require('querystring');
+var url = require('url');
 
 // Configure application
-app.set('json escape','true');
+app.set('json escape', 'true');
 app.set('title', 'Message Board');
 //app.use(express.static(path.join(__dirname,'static'))); // allow direct access to this folder
 
@@ -19,11 +22,11 @@ app.set('title', 'Message Board');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 3000;        // set our port
 
 // Set the MongoDb Database connection
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bears');
+var mongoose = require('mongoose');
+mongoose.createConnection('mongodb://localhost:27017/bears');
 
 // Define model
 //var Image = require('./app/models/image');
@@ -34,86 +37,53 @@ mongoose.connect('mongodb://localhost:27017/bears');
 var router = express.Router(); // get instance of Router
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
     // do logging
-    console.log('('+new Date().toLocaleString()+') Remote user ('+(req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress)+') requesting \''+req.method+": "+req.originalUrl+'\'.');
+    console.log('(' + new Date().toLocaleString() + ') Remote user (' + (req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress) + ') requesting \'' + req.method + ": " + req.originalUrl + '\'.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
+const nasaApiUrl = "images-api.nasa.gov";
+const searchPath = "/search";
+const collectionPath = "/asset";
+const nasaApiPort = 80;
 
-
-
-
-
-
-// on routes that end in /messages
+// on routes that end in /images
 // ----------------------------------------------------
 router.route('/images')
 
-    // create a message (./api/messages)
-    .post(function(req, res) {
-        // var image = new Image();      // create a new instance of the Image model
-        // message.course = req.body.course;  // set the message's course
-        // message.body = req.body.body;  // set the message's body
-        // message.date = new Date();  // set the message's course
-        // console.log(req);
-        ////save the bear and check for errors
-        // message.save(function(err) {
-            // if (err)
-                // res.send(err);
+    // get all the images from API
+    .get(function (req, res) {
+        const options = {
+            hostname: nasaApiUrl,
+            path: searchPath + "?" + querystring.stringify(
+                { 
+                    "q": "*",
+                    "media_type": "image" 
+                }),
+            method: 'GET'
+        };
+        
+        const reqApi = http.request(options, (resApi) => {
+            // resApi.setEncoding('utf8');
+            let rawData = '';
+            resApi.on('data', (chunk) => {
+                rawData+=chunk;
+            });
+            resApi.on('end', () => {
+                const parsedData = JSON.parse(rawData);
+                res.json(parsedData);
+            });
+        });
 
-            // res.json({ message: 'Message created!' });
-        // });
+        reqApi.on('error', (e) => {
+            console.error(`Problem with request: ${e.message}`);
+        });
 
-    })
-    
-    // get all the messages
-    .get(function(req, res) {
-        // Message.find()
-        // .sort({ date : -1 })
-        // .limit(20)
-        // .exec(function(err, messages) {
-            // if (err)
-                // res.send(err);
+        reqApi.end();
 
-            // res.json(messages);
-        // });
     });
 
-router.route('/images/:id')
-
-	// get the image with this id
-	.get(function(res,res){
-		
-	});
-    
-// on routes that end in /messages/:course_id
-// ----------------------------------------------------
-router.route('/collection/:id')
-
-    // get the message with that course ID
-    .get(function(req, res) {
-        // Message
-        // .find({'course': req.params.course})
-        // .sort({ date : -1 })
-        // .limit(20)
-        // .exec(function(err, messages) {
-            // if (err)
-                // res.send(err);
-            // res.json(messages);
-        // });
-    })
-    .delete(function(req,res){
-        // Message
-        // .remove({ course: req.params.course })
-        // .exec(function(err) {
-            // if (err)
-                // res.send(err);
-            // res.json({ message: 'Messages deleted!' });
-        // });
-    });
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
 
